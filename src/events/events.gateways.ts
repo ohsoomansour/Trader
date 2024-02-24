7e492a43b3d56d035f04e710e62206fe1f422f54
ohsoomansour <ceoosm@naver.com>
2024-02-24 23:07:42 +0900

    Conference Testing

--- src/events/events.gateways.ts
+++ src/events/events.gateways.ts
@@ -1,155 +1,4 @@
 /* eslint-disable prettier/prettier */
-
-//import { Server, WebSocket } from 'ws'; VS import { Server, Socket } from 'socket.io';
-/* #1.NestJS에서 웹 소켓 서버에 설정 
-1. 설치 및 설정 
- [ Gateways ]
- npm i --save @nestjs/websockets @nestjs/platform-socket.io (*nestjs 공식 문서)
- [ Adapters ]
- npm i -s @nestjs/websockets @nestjs/platform-ws
- 
-2. [main.ts ]에서 'adapter' 추가 필요
- async function bootstrap() {
-  const app = await NestFactory.create(AppModule); 
-  app.useWebSocketAdapter(new WsAdapter(app));
-  await app.listen(3000);
-}
-bootstrap();
-
- ########################## 웹소켓 설정 문제 ###############
- 샘플 git 참조: https://github.com/nestjs/nest/tree/master/sample/16-gateways-ws/src/events
- 1.해결 방법: npm i ws or npm install @types/ws
- [package.json 파일]
-   "dependencies": {
-    "@nestjs/common": "10.2.10",
-    "@nestjs/core": "10.2.10",
-    "@nestjs/platform-express": "10.2.10", 
-    "@nestjs/platform-ws": "10.2.10", ✅
-    "@nestjs/websockets": "10.2.10", ✅
-    "class-transformer": "0.5.1",
-    "class-validator": "0.14.0",
-    "rimraf": "5.0.5",
-    "reflect-metadata": "0.1.13",
-    "rxjs": "7.8.1",
-    "ws": "8.13.0" ✅
-  }, 
- 
- Q. 웹소켓 각 namespace 어떻게 사용할 것인가 ?
- A. ws library does not support namespaces (communication channels popularised by socket.io).
-   However, to somehow mimic this feature, you can mount multiple ws servers on different paths
-    (example: @WebSocketGateway({ path: '/users' })).
-
- 🚫지원하지 않는 아래의 예시
-  @WebSocketGateway(8080, {
-  namespace: 'chat',
-  cors: { origin: '*' },
-  })
- 
- ✅ 현재 기준은 @WebSocketGateway(8080, {path: '/chat'}) 이렇게 써야된다. 
-   
-
- # @SubscribeMessage(키값) 사용법 
-   1. 'events' 정의한 키값이 존재한 메시지가 도착하면
-      [클라이언트 예시]
-      wsProp.current?.send(JSON.stringify({
-      "event": "events",
-      "data": "12.26 14:03 First Message will arrive at Dev_BackEnd server"
-      }))
-
-      [서버]
-      @SubscribeMessage('events')
-      event키 값은 events라고 인식하고 @MessageBody() data를 수신 
-  */
- /*
- #2Lifecycle hooks 활용
- #3채팅 서버 설계 ( 여기에 시간 투자 )
- 1. redis활용을 통한  https://velog.io/@1yongs_/Redis-Clustering-NestJS-Chat-App
-    - https://github.com/dlfdyd96/nestjs-redis-socketio
-    - 설명: 서버2개, 각 서버에서 채팅을 하는 것을 구현 따라서 웹소켓의 정의를 제대로 구현
-   Redis를 쓰는 이유? 
-    "Key, value 구조의 비정형 데이터를 저장하고 관리하기 위한 오픈 소스 기반의 비관계형 DBMS"
-    >  1개의 서버 이상을 사용 할 때 유용 
-    >  메모리 기반 데이터 구조의 빠른 응답성을 확보함과 동시에 데이터 불일치 문제를 해결
-    > ⭐캐시 서버로 이용할 수 있는 것이 바로 Redis 
-       캐시서버란? "데이터를 가까운 곳에 데이터를 임시 저장" (java, HashMap 원리)
-   [redis DB 설치 및 설정] 
-    > 레디스의 GUI 툴: set d test  "key가 d, value가 test의 값을 생성"
-   
-  
-   [redis와 typeORM과 캐싱 설정]
-   방법1. TypeOrm 
-   TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '284823', //postgresql은 비번을 묻지 않음
-      database: 'NestJS_BackendDev',
-      synchronize: true,
-      logging: true,
-      entities: [Member, Admin],
-      cache: {
-          type: "redis",
-          options: {
-              host: "localhost",
-              port: 6379
-          }
-      }
-    }),
-    
-    
-    방법2. CacheModule 사용
-    *참고: https://show400035.tistory.com/188#google_vignette
-    > import { Module, CacheModule } from '@nestjs/common';
-    > 
-    > class-validator 설치: npm i --save class-validator class-transformer
-    > 컨틀로러 > 서비스 > redis
-     constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
-     const value = await this.cacheManager.get(key);
-  */ 
- /*
-  * #WebRTC 구현: 
-     https://acstory.tistory.com/534#google_vignette 참조
-     socket.io - DOCS 참조: https://socket.io/docs/v4/server-api/ 
-     room 참가 방식 참조: https://surprisecomputer.tistory.com/9
-     + nestjs chatRoom.service.ts : https://blog.ewq.kr/41 참조
-
-  아래의 3가지 경우 참조: https://velog.io/@fejigu/Socket.IO-client 
-  1. socket.io WebSocket과 함께 작동하는 library:  브로드캐스팅을 지원
-   ✅ 웹 소켓은 socket.io에서 사용하는 to 기능, room기능이 없음  
-      > "즉 room을 만들어서 써야되고 그리고 프론트 socket-io-client에서 보낸 socket을 웹소켓 안에서 사용 가능"
-        프론트엔드 socket -> BE 웹소켓 연동이 가능하다!
-  
-   [Public ] : "연결된 모든 클라이언트에게 보냄, 채팅 메시지가 적절"
-   <서버 측>
-  io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-      io.emit('chat message', msg);  // ✅연결된 모든 클라이언트에게 메시지 통신(브로드 캐스트)
-  
-    });
-  });
-  <클라이언트 측> 
-  socket.on('chat message', (msg) => {
-    console.log(`Received message: ${msg}`);
-  });
-
-  [Private] : "특정 고객에게 메시지를 보냄, 예를들어 알림"
-   <서버 측>
-  io.on('connection', (socket) => {
-    socket.on('send notification', (msg, ✅recipientId) => {
-      io.to(recipientId).emit('notification', msg);  // ✅지정된 수신자에게 메시지 보내기
-  
-    });
-  });
-  <클라이언트 측> 
-  socket.on('notification', (msg) => {
-    console.log(`Received notification: ${msg}`);
-  });
-  [Broadcasting] : 발신자를 제외한 모든 클라이언트에게 메세지가 전송되는 경우
-
-  #room이란? "여러 소켓들이 참여(join)하고 떠날 수 있는(leave) 채널"
-   - socket이 connect 될 때 기본적으로 해당 소켓 id이름의 room에 기본적으로 들어가있다.
-*/
 import { Body, Logger } from '@nestjs/common';
 import {
   ConnectedSocket,
@@ -212,41 +61,53 @@ export class EventGateway implements OnGatewayInit, OnGatewayConnection, OnGatew
     this.connectedClients.delete(client.id);
   } 
   @SubscribeMessage('join_room')
-  rtc_joinRoom(@MessageBody() roomId, @ConnectedSocket() client: Socket){
+  rtc_joinRoom(@MessageBody() roomId, @ConnectedSocket() socket: Socket){
     //private conferenceRoomToSockets: { [roomId: string]: Socket[] } = {};
     console.log(roomId);
     this.logger.log('join_room');  
+    socket.join(roomId);
+    socket.to(roomId).emit("welcome", "new Peer Join!")
+
+    /* 
     if (!this.conferenceRoomToSockets[roomId]) {
       this.conferenceRoomToSockets[roomId] = [];
     }
-    //전화를 걸고 싶은 방을 고르는 중
-    this.conferenceRoomToSockets[roomId].push(client);
+    this.conferenceRoomToSockets[roomId].push(socket);
     this.conferenceRoomToSockets[roomId].forEach((s) => {
       s.emit("welcom", "Hi! Welcom!!")
     })
+    */
   }
+
   @SubscribeMessage('offer')
-  rtc_receiveOffer(@MessageBody() info) {
+  rtc_receiveOffer(@MessageBody() offerInfo, @ConnectedSocket() socket: Socket) {
+    this.logger.log('offer');
+    socket.to(offerInfo[1]).emit("offer", offerInfo[0]);
+    /*
     this.conferenceRoomToSockets[info[1]].forEach((s) => {
       s.emit("offer", info[0])
-    })
+    })*/
   }
-
   @SubscribeMessage('answer')
-  rtc_receiveAnswer(@MessageBody() info) {
-    this.logger.log(info);
-    console.log(info)
+  rtc_receiveAnswer(@MessageBody() answerInfo, @ConnectedSocket() socket: Socket) {
+    this.logger.log('answer');
+    socket.to(answerInfo[1]).emit("answer", answerInfo[0])
+    
+    /*
     this.conferenceRoomToSockets[info[1]].forEach((s) => {
       s.emit("answer", info[0])
     })
+    */
   }
   @SubscribeMessage("ice")
-  rtc_receiveICEcandidate(@MessageBody() info) {
+  rtc_receiveICEcandidate(@MessageBody() ICEinfo, @ConnectedSocket() socket: Socket) {
+    socket.to(ICEinfo[1]).emit("ice", ICEinfo[0]);
+    /*
     this.conferenceRoomToSockets[info[1]].forEach((s) => {
-      s.emit("answer", info[0])
+      s.emit("ice", info[0])
     })
+    */
   }
-
   //===========================  ============================================================
   @SubscribeMessage('join')
   handleEmit(@MessageBody() roomId: any, @ConnectedSocket() client: Socket) {
