3a4b074f72352cd68c85caecac324352a7430322
ohsoomansour <ceoosm@naver.com>
2024-03-13 17:23:31 +0900

    CORS TESTing..

--- src/main.ts
+++ src/main.ts
@@ -77,7 +77,7 @@ async function bootstrap() {
     (req, res, next) => {
       res.header('Access-Control-Allow-Origin', '*');
       res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-      //res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
       next();
     },
   );
